3224a9be66bf9b86e7efe47366025d4a94339d77
Nikolai Mavrenkov <koluch@koluch.ru>
2015-07-07 22:09:01 +0300

    Add page title ; fix server

--- guess-russia-regions/sharing.js
+++ guess-russia-regions/sharing.js
@@ -43,7 +43,7 @@ sharing = new function(){
         // facebook loading
         parent.innerHTML += '<div class="widget">'
         + '<div class="fb-share-button"'
-        + ' data-href="https://www.facebook.com/v2.2/dialog/feed?app_id=45075597673&description=I%20got%201%2F50%20right%20%E2%80%93%20can%20you%20do%20any%20better%3F&display=popup&e2e=%7B%7D&link=http%3A%2F%2Fbuzzfeed.com%2Frobinedds%2Fhow-well-do-you-actually-know-the-us-states&locale=en_US&name=How%20Well%20Do%20You%20Actually%20Know%20The%20US%20States%3F&next=http%3A%2F%2Fstatic.ak.facebook.com%2Fconnect%2Fxd_arbiter%2FxRlIuTsSMoE.js%3Fversion%3D41%23cb%3Df1e385bafc%26domain%3Dgames.buzzfeed.com%26origin%3Dhttp%253A%252F%252Fgames.buzzfeed.com%252Ff39a53ca78%26relation%3Dopener%26frame%3Df12974aaf8%26result%3D%2522xxRESULTTOKENxx%2522&picture=http%3A%2F%2Fgames.buzzfeed.com%2F_uk%2Fstate-quiz%2Fimg%2Fflags%2Fflagresult1.jpg&sdk=joey&version=v2.2"'
+        + ' data-href="http://koluch.github.io/guess-russia-regions/"'
         + ' data-layout="button"></div>'
         + '<div id="fb-root"></div>'
         + '</div>';
