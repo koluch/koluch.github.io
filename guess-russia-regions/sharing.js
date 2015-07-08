d3c543fada711c2c0fbee22e42247fc12a1b4607
Nikolai Mavrenkov <koluch@koluch.ru>
2015-07-08 22:14:07 +0300

    Fix FB sharing button

--- guess-russia-regions/sharing.js
+++ guess-russia-regions/sharing.js
@@ -48,14 +48,21 @@ sharing = new function(){
         + ' data-lang="ru"></div>'
         + '<div id="fb-root"></div>'
         + '</div>';
-        (function (d, s, id) {
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '405695916297888',
+                xfbml      : true,
+                version    : 'v2.4'
+            });
+        };
+        (function(d, s, id){
             var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.3";
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/ru_RU/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
+        ;
 
 
 
